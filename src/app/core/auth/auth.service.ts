7ecb4485a70dc71d0afa9e9e1d60b647c7bb9cbb
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-06-16 10:25:08 -0300

    Changed auth.service to get token from body.

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -6,7 +6,7 @@ import { UserService } from '../user/user.service';
 const API_URL = 'http://localhost:3000';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class AuthService {
   constructor(private http: HttpClient, private userService: UserService) {}
@@ -14,14 +14,13 @@ export class AuthService {
   authenticate(email: string, password: string) {
     return this.http
       .post(
-        API_URL + '/session/auth',
+        API_URL + '/auth/token',
         { email, password },
         { observe: 'response' }
       )
       .pipe(
         tap(res => {
-          console.log(res);
-          const authToken = res.headers.get('x-access-token');
+          const authToken = res.body['accessToken'];
           this.userService.setToken(authToken);
           console.log(`User ${email} authenticated with token ${authToken}`);
         })
